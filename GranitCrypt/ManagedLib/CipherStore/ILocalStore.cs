47795003d2da179ab9f8c646f69f195bb32b585e
Bob <789RT@mail.ru>
2023-04-13 21:20:41 +0300

    #7004-добавлена библиотека на C#, добавлен сервис создания хранилища.

---
+++ GranitCrypt/ManagedLib/CipherStore/ILocalStore.cs
@@ -0,0 +1,20 @@
+﻿namespace ManagedLib.CipherStore
+{
+    /// <summary>
+    /// Локальное хранилище кодируемых и декодируемых файлов.
+    /// </summary>
+    internal interface ILocalStore
+    {
+        /// <summary>
+        /// Создает каталог для сохранения зашифрованных файлов.
+        /// </summary>
+        /// <param name="path"></param>
+        void InitEncriptStore(string path);
+
+        /// <summary>
+        /// Создает каталог для сохранения расшифрованных файлов.
+        /// </summary>
+        /// <param name="path"></param>
+        void InitDecriptStore(string path);
+    }
+}
---
+++ GranitCrypt/ManagedLib/CipherStore/StoreNames.cs
@@ -0,0 +1,15 @@
+﻿namespace ManagedLib.CipherStore
+{
+    public class StoreNames
+    {
+        /// <summary>
+        /// Имя локального каталога хранения шифрованных файлов.
+        /// </summary>
+        public const string LocalStoreEncryptName = "Encrypted_files";
+
+        /// <summary>
+        /// Имя локального каталога хранения шифрованных файлов.
+        /// </summary>
+        public const string LocalStoreDecryptName = "Decrypted_files";
+    }
+}
